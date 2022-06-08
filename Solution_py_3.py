508d45fd77f6dcd43eec1d5497cfb6c236db1320
RISHIKESHdev <rishikeshhappydeveloper@gmail.com>
2022-06-08 21:27:36 +0530

    day2

---
+++ Solution_py_3.py
@@ -0,0 +1,16 @@
+def leastcostStair(arr):
+    n=len(arr)
+    db=[None]*n
+    return (min(costStair(n-1,arr,db),costStair(n-2,arr,db)))
+def costStair(n,arr,db):
+    if(n<0):
+        return 0
+    if(n==0 or n==1):
+        return arr[n]
+    if(db[n]):
+             return(db[n])
+
+    ab=arr[n]+min(costStair(n-1,arr,db),costStair(n-2,arr,db))
+    return (ab)
+arr = [ int(x.strip()) for x in input().split(",")]
+print(leastcostStair(arr))
