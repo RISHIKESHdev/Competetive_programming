85ed117a85af516a6e4bb2e47cc55ee323b5a594
RISHIKESHdev <rishikeshhappydeveloper@gmail.com>
2022-06-11 21:08:38 +0530

    day5

--- Array/1_20/Solution_py_6.py
+++ Array/1_20/Solution_py_6.py
@@ -1,10 +1,23 @@
-def backspaceStr(s):
+def backspaceStr(s,t):
     s_1=""
     t_1=""
-    map(lambda n: [s_1.append(n) if(n!="#") else s_1.rstrip(s_1[-1])], s)
-    map(lambda n: [t_1.append(n) if(n!="#") else t_1.rstrip(t_1[-1])], t)
-    return(True if(s_1==t_1) else False)
+    if(s==t):
+        return True
+    for i in s:
+        if (i=="#"):
+            if (s_1!=""):
+                s_1=s_1[:len(s_1)-1]
+        else:
+            s_1=s_1+i
+    for j in t:
+        if (j=="#"):
+            if (t_1!=""):
+                t_1=t_1[:len(t_1)-1]
+        else:
+            t_1=t_1+j
+    print(s_1,t_1)
+    return(s_1==t_1)
 
 s = input()
 t = input()
-print(backspaceStr(s))
+print(backspaceStr(s,t))
