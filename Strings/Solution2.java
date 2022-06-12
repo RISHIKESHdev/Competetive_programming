268b60d173fcfcca680ddb6312ad45032cba24d3
vasanthkannanr <74522107+vasanthkannanr@users.noreply.github.com>
2022-06-12 20:25:40 +0530

    Update Solution2.java

--- Strings/Solution2.java
+++ Strings/Solution2.java
@@ -2,21 +2,20 @@ class Solution {
     public String removeOuterParentheses(String s) {
         int start=0;
         int end=0;
-        int count1=0;
-        int count2=0;
+        int count=0;
         StringBuilder str=new StringBuilder();
         for(int i=0;i<s.length();i++){
             if(s.charAt(i)=='('){
-                count1++;
+                count++;
             }else if(s.charAt(i)==')'){
-                count2++;
+                count--;
             }
             
-            if(count1==count2){
+            if(count==0){
                 end=i;
                 str.append(s.substring(start+1,end));
                 start=end+1;
-                count1=0;count2=0;
+                count=0;
             }
         }return str.toString();
     }
