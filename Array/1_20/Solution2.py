81e34d03305f16917b3981fbf951da4b33142689
RISHIKESHdev <rishikeshhappydeveloper@gmail.com>
2022-06-12 14:57:08 +0530

    day6

--- Array/1_20/Solution2.py
+++ Array/1_20/Solution2.py
@@ -1,16 +1,28 @@
 def findTwoSum(nums, target):
-  try:
-    numsMap = [None]*target
-    for p in range(0,len(nums)):
-      currentMapVal = numsMap[nums[p]];
-      if(currentMapVal or currentMapVal==0) :
-        return (currentMapVal, p);
-      else :
-        numberToFind = target - nums[p];
-        numsMap[numberToFind] = p;
-    return
-  except:
-   return "Invalid input array"
+  d = {}
+  n = len(nums)
+  for i in range(n):
+    m = target - nums[i]
+    if m in d:
+      return [d[m],i]
+    d[nums[i]] = i
 arr = [ int(x.strip()) for x in input().split(",")]
 target=int(input())
-print([findTwoSum(arr, target) if target>0 else "Invalid target"]);
+print(findTwoSum(arr, target));
+
+# def findTwoSum(nums, target):
+#   try:
+#     numsMap = [None]*target
+#     for p in range(0,len(nums)):
+#       currentMapVal = numsMap[nums[p]];
+#       if(currentMapVal or currentMapVal==0) :
+#         return (currentMapVal, p);
+#       else :
+#         numberToFind = target - nums[p];
+#         numsMap[numberToFind] = p;
+#     return
+#   except:
+#    return "Invalid input array"
+# arr = [ int(x.strip()) for x in input().split(",")]
+# target=int(input())
+# print([findTwoSum(arr, target) if target>0 else "Invalid target"]);
