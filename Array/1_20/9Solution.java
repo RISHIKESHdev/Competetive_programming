6c8f5dc43c18b238dda63390d78098fb1406ed19
vasanthkannanr <74522107+vasanthkannanr@users.noreply.github.com>
2022-06-14 15:17:45 +0530

    Create 9Solution.java

---
+++ Array/1_20/9Solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int countOdds(int low, int high) {
+        if(high%2==0&&low%2==0){
+            return (high-low)/2;
+        }else{
+            return (high-low)/2+1;
+        }
+    }
+}
