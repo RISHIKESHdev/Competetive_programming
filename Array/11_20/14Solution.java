abc72b43f47fbc277c1398da8ef05151461bb22f
vasanthkannanr <74522107+vasanthkannanr@users.noreply.github.com>
2022-06-21 18:59:22 +0530

    Update 14Solution.java

--- Array/11_20/14Solution.java
+++ Array/11_20/14Solution.java
@@ -1,1 +1,11 @@
-
+class Solution {
+    public int minimumNumbers(int num, int k) {
+        if(num==0) return 0;
+        for(int i=1;i<=10&&i*k<=num;i++){
+            if(i*k%10==num%10){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
