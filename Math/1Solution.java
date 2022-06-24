c30b6fb9603695ff22d1c59c2e71f523fc23d09a
vasanthkannanr <74522107+vasanthkannanr@users.noreply.github.com>
2022-06-24 17:18:55 +0530

    Create 1Solution.java

---
+++ Math/1Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int climbStairs(int n) {
+        int a=0, b=1;
+        int c=0;
+        for(int i=0;i<n;i++){
+            c=a+b;
+            a=b;
+            b=c;
+        }
+        return c;
+    }
+}
