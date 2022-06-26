2e711e754ca32dff8b7c1aeac43bd639d851aae5
vasanthkannanr <74522107+vasanthkannanr@users.noreply.github.com>
2022-06-26 18:28:53 +0530

    Update 10Solution.java

--- Strings/1_10/10Solution.java
+++ Strings/1_10/10Solution.java
@@ -1,20 +1,19 @@
 class Solution {
     public boolean areAlmostEqual(String s1, String s2) {
+        if(s1.equals(s2)) return true;
         int count=0,index=0;
         char[] arr=new char[4];
         for(int i=0;i<s1.length();i++){
             if(s1.charAt(i)!=s2.charAt(i)){
-                if(count==0){
-                    arr[index++]=s1.charAt(i);
-                    arr[index++]=s2.charAt(i);
-                }if(count==1){
-                    arr[index++]=s1.charAt(i);
-                    arr[index++]=s2.charAt(i);
-                }
                 count++;
+                if(count>2){
+                    return false;
+                }
+                arr[index++]=s1.charAt(i);
+                arr[index++]=s2.charAt(i);
             }
         }
-        if(count==0||(count==2&&arr[0]==arr[3]&&arr[1]==arr[2])){
+        if(arr[0]==arr[3]&&arr[1]==arr[2]){
             return true;
         }
         return false;
