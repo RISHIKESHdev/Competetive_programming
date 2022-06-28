eacf797962845196c5d3fa062a291fb8c06abc05
RISHIKESHdev <rishikeshhappydeveloper@gmail.com>
2022-06-28 23:29:35 +0530

    day

---
+++ Strings/2_10/12Solution.py
@@ -0,0 +1,9 @@
+def removeDuplicates(S):
+        stack = []
+        for c in S:
+            if stack and stack[-1] == c:
+                stack.pop()
+            else:
+                stack.append(c)
+        return ''.join(stack)
+print(removeDuplicates(input()))
