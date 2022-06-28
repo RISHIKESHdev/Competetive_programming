ffda3d8dda6b34da14d2f8a7e3adccdb96708990
vasanthkannanr <74522107+vasanthkannanr@users.noreply.github.com>
2022-06-28 13:15:37 +0530

    Update 19Solution.java

--- Array/11_20/19Solution.java
+++ Array/11_20/19Solution.java
@@ -1,23 +1,16 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i=0,j=0;
-        int[] arr=new int[m+n];
-        int pos=0;
-        while(i<m&&j<n){
-            if(nums1[i]<=nums2[j]){
-                arr[pos]=nums1[i++];
+        int i=m-1,j=n-1;
+        int pos=m+n-1;
+        while(i>=0&&j>=0){
+            if(nums1[i]>=nums2[j]){
+                nums1[pos]=nums1[i--];
             }else{
-                arr[pos]=nums2[j++];
-            }pos++;
+                nums1[pos]=nums2[j--];
+            }pos--;
         }
-        while(i<m){
-            arr[pos++]=nums1[i++];
-        }while(j<n){
-            arr[pos++]=nums2[j++];
+        while(j>=0){
+            nums1[pos--]=nums2[j--];
         }
-        for(i=0;i<m+n;i++){
-            nums1[i]=arr[i];
-        }
-        
     }
 }
