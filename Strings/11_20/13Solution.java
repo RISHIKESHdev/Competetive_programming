9aa684edf6c70e9385ce9c5e0bbd93208a390996
vasanthkannanr <74522107+vasanthkannanr@users.noreply.github.com>
2022-07-04 12:43:51 +0530

    Update 13Solution.java

--- Strings/11_20/13Solution.java
+++ Strings/11_20/13Solution.java
@@ -4,19 +4,19 @@ class Solution {
         if(len>s2.length()){
             return false;
         }
-        int[] arr=new int[26];
         int[] arr1=new int[26];
+        int[] arr2=new int[26];
         for(int i=0;i<len;i++){
-            arr[s1.charAt(i)-'a']++;
-            arr1[s2.charAt(i)-'a']++;
+            arr1[s1.charAt(i)-'a']++;
+            arr2[s2.charAt(i)-'a']++;
         }
         for(int i=0;i<s2.length()-len;i++){
-            if(Arrays.equals(arr,arr1)){
+            if(Arrays.equals(arr1,arr2)){
                 return true;
             }
-            arr1[s2.charAt(i+len)-'a']++;
-            arr1[s2.charAt(i)-'a']--;
+            arr2[s2.charAt(i+len)-'a']++;
+            arr2[s2.charAt(i)-'a']--;
         }
-        return Arrays.equals(arr,arr1);
+        return Arrays.equals(arr1,arr2);
     }
 }
