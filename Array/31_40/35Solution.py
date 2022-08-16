da084747adeddc91ca25e3fd4fd2ad68c6af46bb
RISHIKESHdev <rishikeshhappydeveloper@gmail.com>
2022-08-16 23:15:23 +0530

    35

---
+++ Array/31_40/35Solution.py
@@ -0,0 +1,13 @@
+def firstMissingPositive(nums):
+        nums.append(0)
+        n = len(nums)
+        for i in range(n):
+            if not (1 <= nums[i] < n):
+                nums[i] = 0
+        for i in range(n):
+            nums[nums[i] % n] += n
+        for i in range(1, n):
+            if nums[i] // n == 0:
+                return i
+        return n
+nums = [ int(x.strip()) for x in input().split(",")]
